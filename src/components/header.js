92fabdb3652920edcdab7f4a2ddcee66a5829801
Kalene Csefalvay <kalenecsefalvay@Kalenes-MacBook-Pro.local>
2020-06-28 17:48:54 -0400

    adds effects for svg resize

--- src/components/header.js
+++ src/components/header.js
@@ -1,45 +1,66 @@
-import React from "react"
+import React, { useState, useRef, useEffect} from "react"
 import NameTag from './nametag'
 import Navigation from './navigation'
 import headerStyles from './style-modules/header.module.css'
 import MeAndReese from '../images/me-and-reese-icon.jpg'
 
-const Header = _props => (
-    <header>
-        <Navigation></Navigation>
-        <NameTag></NameTag>
+function Header() {
+    const [svgSize, setSvgSize] = useState(null)
+    const imageRef = useRef();
+    const imageContainer = useRef()
 
-        <div className={headerStyles.header__description_container}>
-            <div className={headerStyles.header__description}>
-                <h2>Hi.</h2>
-                <p>I’m Kalene, a frontend developer and lifelong-learner with a desire to help support others in, or looking to get into tech.</p>
-                <p>Read more about my journey <a href="#">here</a>.</p>
-            </div>
+    // @TODO: clean up to removoe effect when componentWillUnmount()
+    useEffect(() => {
+        function handleResize() {
+            const imageContainerWidth = imageContainer.current.getBoundingClientRect().width;
+            setSvgSize(imageContainerWidth)
+        }
 
-            <div className={headerStyles.header__image_container}>
-                <img src={MeAndReese} alt="Kalene and her dog, Reese."/>
-                <div class={headerStyles.svg__container}>
-                    <svg className={headerStyles.svg__one} width="100" height="100" xmlns="http://www.w3.org/2000/svg" opacity="0.5">
-                        <rect width="100" height="100" />
-                    </svg>
-                    <svg className={headerStyles.svg__two}  width="100" height="100" xmlns="http://www.w3.org/2000/svg" opacity="0.5">
-                        <rect width="100" height="100" />
-                    </svg>
-                    <svg className={headerStyles.svg__three}  width="100" height="100" xmlns="http://www.w3.org/2000/svg" opacity="0.5">
-                        <rect width="100" height="100" />
-                    </svg>
+        window.document.addEventListener('resize', handleResize)
+
+        handleResize()
+    })
+
+            
+    return (
+        
+        <header>
+            <Navigation></Navigation>
+            <NameTag></NameTag>
+
+            <div className={headerStyles.header__description_container}>
+                <div className={headerStyles.header__description}>
+                    <h2>Hi.</h2>
+                    <h2>svgSize: {svgSize}</h2>
+                    <p>I’m Kalene, a frontend developer and lifelong-learner with a desire to help support others in, or looking to get into tech.</p>
+                    <p>Read more about my journey <a href="#">here</a>.</p>
+                </div>
+
+                <div ref={imageContainer} className={headerStyles.header__image_container}> 
+                    {/* <div className={headerStyles.svg__container} style={ {height: `${svgSize}px`} } > */}
+ 
+                    <div className={headerStyles.svg__container} >
+
+                        <svg className={headerStyles.svg__one} width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.5">
+                            <rect width={svgSize} height={svgSize} />
+                        </svg>
+                        <svg className={headerStyles.svg__two}  width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.5">
+                            <rect width={svgSize} height={svgSize} />
+                        </svg>
+                        <svg className={headerStyles.svg__three}  width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.5">
+                            <rect width={svgSize} height={svgSize} />
+                        </svg>
+                    </div>
+                    <img ref={imageRef} src={MeAndReese} alt="Kalene and her dog, Reese."/>
+
                 </div>
             </div>
-            
-        </div>
 
-        
+            <hr/>
 
-        <hr/>
+        </header>
+    )
 
-        
-    </header>
-
-)
+}
 
 export default Header;
