4a4560b29d8e415e0c01bdb9bb4da47ba47b120d
Kalene Csefalvay <kalenecsefalvay@Kalenes-MacBook-Pro.local>
2020-06-29 21:01:16 -0400

    adds breakpoints for larger screen sizes

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,8 @@ import headerStyles from './style-modules/header.module.css'
 import MeAndReese from '../images/me-and-reese-icon.jpg'
 
 function Header() {
+    // Proposed breakpoint for header: 980px
+
     const [svgSize, setSvgSize] = useState(null)
     const imageRef = useRef();
     const imageContainer = useRef()
@@ -12,7 +14,7 @@ function Header() {
     // @TODO: clean up to removoe effect when componentWillUnmount()
     useEffect(() => {
         function handleResize() {
-            const imageContainerWidth = imageContainer.current.getBoundingClientRect().width;
+            const imageContainerWidth = imageContainer.current.getBoundingClientRect().height;
             setSvgSize(imageContainerWidth)
             console.log('handleResize called; svgSize: ', svgSize)
         }
@@ -32,27 +34,25 @@ function Header() {
             <div className={headerStyles.header__description_container}>
                 <div className={headerStyles.header__description}>
                     <h2>Hi.</h2>
-                    <h2>svgSize: {svgSize}</h2>
                     <p>I’m Kalene, a frontend developer and lifelong-learner with a desire to help support others in, or looking to get into tech.</p>
                     <p>Read more about my journey <a href="#">here</a>.</p>
                 </div>
 
                 <div ref={imageContainer} className={headerStyles.header__image_container}> 
-                    {/* <div className={headerStyles.svg__container} style={ {height: `${svgSize}px`} } > */}
  
                     <div className={headerStyles.svg__container} >
 
-                        <svg className={headerStyles.svg__one} width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.5">
-                            <rect width={svgSize} height={svgSize} />
+                        <svg className={headerStyles.svg__one} width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.1">
+                            <rect width={svgSize} height={svgSize} fill="var(--light-olive)" />
                         </svg>
-                        <svg className={headerStyles.svg__two}  width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.5">
-                            <rect width={svgSize} height={svgSize} />
+                        <svg className={headerStyles.svg__two}  width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.1">
+                            <rect width={svgSize} height={svgSize} fill="var(--light-olive)" />
                         </svg>
-                        <svg className={headerStyles.svg__three}  width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.5">
-                            <rect width={svgSize} height={svgSize} />
+                        <svg className={headerStyles.svg__three}  width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.1">
+                            <rect width={svgSize} height={svgSize} fill="var(--light-olive)" />
                         </svg>
                     </div>
-                    <img ref={imageRef} src={MeAndReese} alt="Kalene and her dog, Reese."/>
+                    <img style={{width: `${svgSize * 0.85}px`}} ref={imageRef} src={MeAndReese} alt="Kalene and her dog, Reese."/>
 
                 </div>
             </div>
