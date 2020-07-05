f019606d7fa7463992c577b3654f346cb6b248a7
Kalene Csefalvay <kalenecsefalvay@Kalenes-MacBook-Pro.local>
2020-07-05 19:05:20 -0400

    fixes handleResize being called in an infinite loop

--- src/components/header.js
+++ src/components/header.js
@@ -7,22 +7,76 @@ import MeAndReese from '../images/me-and-reese-icon.jpg'
 function Header() {
     // Proposed breakpoint for header: 980px
 
-    const [svgSize, setSvgSize] = useState(null)
-    const imageRef = useRef();
-    const imageContainer = useRef()
-
-    // @TODO: clean up to removoe effect when componentWillUnmount()
-    useEffect(() => {
-        function handleResize() {
-            const imageContainerWidth = imageContainer.current.getBoundingClientRect().height;
-            setSvgSize(imageContainerWidth)
-            console.log('handleResize called; svgSize: ', svgSize)
+    const [svgConfig, setSvgConfig] = useState(
+        {   
+            svgSize: null, 
+            svgPosition: { 
+                topOffset: null,
+                leftOffset: null,
+            }
         }
+    )
+    const imageRef = useRef();
+    const imageContainer = useRef();
+
+    // const handleResize = () => {
+    //     const svgSize = imageRef.current.getBoundingClientRect().height;
+    //     const topOffset = `${imageRef.current.offsetTop}px`
+    //     const leftOffset = `${imageRef.current.offsetLeft}px`
+
+    //     const newState = {
+    //         ...svgConfig,
+    //         svgSize,
+    //         svgPosition: {
+    //             ...svgConfig.svgPosition,
+    //             topOffset,
+    //             leftOffset
+    //         }
+    //     }
+
+    //     setSvgConfig(newState);
+
+    //     console.log('handleResize called')
+    //     console.log('svgConfig', svgConfig)
+    // }
+
+    // --- Effects ---
+    // useEffect(() => {
+    //     window.addEventListener('resize', handleResize)
+
+    //     return _ => {
+    //         window.removeEventListener('resize', handleResize)
+    //     } 
+    // })
+
+    // Send in empty array to only run on mount and unmount of component
+    useEffect(() => {
+
+        let newState;
+        const handleResize = () => {
+            const svgSize = `${imageRef.current.getBoundingClientRect().height}`;
+            const topOffset = `${imageRef.current.offsetTop}px`
+            const leftOffset = `${imageRef.current.offsetLeft}px`
+    
+            newState = {
+                svgSize,
+                svgPosition: {
+                    topOffset,
+                    leftOffset
+                }
+            }
+    
+            setSvgConfig(newState);
+            console.log('handleResize called')
+        }
+
+
+        handleResize()
 
         window.addEventListener('resize', handleResize)
 
-        handleResize()
-    })
+    }, [])
+
 
             
     return (
@@ -39,19 +93,39 @@ function Header() {
                 </div>
 
                 <div ref={imageContainer} className={headerStyles.header__image_container}> 
- 
-                    <div className={headerStyles.svg__container} >
+                    {/* <div className={headerStyles.svg__container} > */}
 
-                        <svg className={headerStyles.svg__one} width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.1">
-                            <rect width={svgSize} height={svgSize} fill="var(--light-olive)" />
+                        <svg 
+                            className={headerStyles.svg__one} 
+                            width={svgConfig.svgSize} 
+                            height={svgConfig.svgSize} 
+                            style={ {top: svgConfig.svgPosition.topOffset, left: svgConfig.svgPosition.leftOffset } }
+                            xmlns="http://www.w3.org/2000/svg" 
+                            opacity="0.1"
+                        >
+                            <rect width={svgConfig.svgSize} height={svgConfig.svgSize} fill="var(--light-olive)" />
                         </svg>
-                        <svg className={headerStyles.svg__two}  width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.1">
-                            <rect width={svgSize} height={svgSize} fill="var(--light-olive)" />
+                        <svg 
+                            className={headerStyles.svg__two}  
+                            width={svgConfig.svgSize} 
+                            height={svgConfig.svgSize} 
+                            style={ {top: svgConfig.svgPosition.topOffset, left: svgConfig.svgPosition.leftOffset } }
+                            xmlns="http://www.w3.org/2000/svg" 
+                            opacity="0.1"
+                        >
+                            <rect width={svgConfig.svgSize} height={svgConfig.svgSize} fill="var(--light-olive)" />
                         </svg>
-                        <svg className={headerStyles.svg__three}  width={svgSize} height={svgSize} xmlns="http://www.w3.org/2000/svg" opacity="0.1">
-                            <rect width={svgSize} height={svgSize} fill="var(--light-olive)" />
+                        <svg 
+                            className={headerStyles.svg__three}  
+                            width={svgConfig.svgSize} 
+                            height={svgConfig.svgSize} 
+                            style={ {top: svgConfig.svgPosition.topOffset, left: svgConfig.svgPosition.leftOffset } }
+                            xmlns="http://www.w3.org/2000/svg" 
+                            opacity="0.1"
+                        >
+                            <rect width={svgConfig.svgSize} height={svgConfig.svgSize} fill="var(--light-olive)" />
                         </svg>
-                    </div>
+                    {/* </div> */}
                     <img ref={imageRef} src={MeAndReese} alt="Kalene and her dog, Reese."/>
 
                 </div>
