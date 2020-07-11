a3a4eaae9daefc7c980d66e5059d8a40e6bbac6c
Kalene Csefalvay <kalenecsefalvay@Kalenes-MacBook-Pro.local>
2020-07-11 17:39:36 -0400

    adds footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,18 @@
 import React from "react"
+import footerStyles from './style-modules/footer.module.css'
 
-const Footer = _props => (
-    <footer>footer</footer>
-)
+const Footer = _props => {
+    const today = new Date();
+    const fullYear = today.getFullYear();
+
+
+    return (
+        <footer className={footerStyles.red}>
+            <p>&copy; {fullYear} Kalene Csefalvay </p>
+        </footer>
+    )
+}
+    
+
 
 export default Footer;
