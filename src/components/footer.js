e5d0451f673073e953738f8ce94fb4b76376303a
Kalene Csefalvay <kalenecsefalvay@Kalenes-MacBook-Pro.local>
2020-12-22 22:17:46 -0500

    imports icons with react-icons package

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { FaTwitter, FaLinkedin, FaGithub, FaCodepen, FaDev } from 'react-icons/fa';
 import footerStyles from './style-modules/footer.module.css'
 
 const Footer = _props => {
@@ -8,11 +9,17 @@ const Footer = _props => {
 
     return (
         <footer className={footerStyles.red}>
+            <ul>
+                <li><FaTwitter /></li>
+                <li><FaLinkedin /></li>
+                <li><FaGithub /></li>
+                <li><FaCodepen /></li>
+                <li><FaDev /></li>
+            </ul>
             <p>&copy; {fullYear} Kalene Csefalvay </p>
         </footer>
     )
 }
-    
 
 
 export default Footer;
